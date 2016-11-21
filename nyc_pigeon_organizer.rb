94255740a21c746be3a1cf944336f28354e3caf6
Cathy Yee <catyyee@hotmail.com>
2016-11-21 23:38:53 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |attribute, info|
+    # attribute = :color, :gender, :lives  info = :purple, :male, :subway
+    info.each do |key, name|
+      # key = :purple, :male, :subway  name = ["Theo", "Peter Jr.", "Lucky"]
+      name.each do |bird|
+        if new_hash[bird] == nil  #if new_hash[bird] == nil means there is no key with that bird
+          new_hash[bird] = {}     #this creates a key with that birds name (e.g. new_hash = {Theo => {}})
+        end
+        if new_hash[bird][attribute] == nil
+          new_hash[bird][attribute] = []      # need to set this to an array
+        end
+        new_hash[bird][attribute] << key.to_s  # need to change the symbol into a string
+      end
+    end
+  end
+  new_hash
 end
